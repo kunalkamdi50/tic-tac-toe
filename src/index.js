70be985cb370bf52cad3d75a62edb5f096c2a29f
kunalkamdi50 <kunalkamdi50@gmail.com>
2024-07-16 16:53:33 +0530

    second

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-import Game from "./App";
+
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Game />
+    <App />
   </StrictMode>
 );
